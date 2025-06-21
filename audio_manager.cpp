c7888c343e38efae1333e6118b99af6396877d7a
phpcalismak <138626283+phpcalismak@users.noreply.github.com>
2025-06-21 22:14:51 +0300

    Update audio_manager.cpp

--- audio_manager.cpp
+++ audio_manager.cpp
@@ -2,17 +2,16 @@
 
 void playSound(int soundType) {
   switch(soundType) {
-    case 1: // Jump sound
+    case 1: 
       tone(BUZZER_PIN, 1000, 50);
       break;
-    case 2: // Hit sound
+    case 2: 
       tone(BUZZER_PIN, 500, 100);
       break;
-    case 3: // Victory sound
+    case 3: 
       tone(BUZZER_PIN, 1500, 100);
-      delay(100); // Small delay between notes
+      delay(100); 
       tone(BUZZER_PIN, 2000, 200);
       break;
-    // Add more sounds as needed
   }
 }
