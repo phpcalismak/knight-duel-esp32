e2b4ab38e124d2355f4c249cfd0790385f716adf
phpcalismak <138626283+phpcalismak@users.noreply.github.com>
2025-06-21 22:07:00 +0300

    Update joystick_manager.h

--- joystick_manager.h
+++ joystick_manager.h
@@ -2,17 +2,17 @@
 #define JOYSTICK_MANAGER_H
 
 #include <Arduino.h>
-#include "game_config.h" // For joystick pin definitions
+#include "game_config.h" 
 
-// External declarations for joystick center values
+
 extern int joy1XCenter;
 extern int joy1YCenter;
 extern int joy2XCenter;
 extern int joy2YCenter;
 
-// Function prototypes
+
 void calibrateJoysticks();
 void printDebugInfo();
-bool isAttackPressed(int btnPin); // <-- ADD THIS LINE
+bool isAttackPressed(int btnPin); 
 
 #endif // JOYSTICK_MANAGER_H
